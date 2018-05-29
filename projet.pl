e511b2fa55f6d0cfe9b7aadf8a7bbb9f20e658d3
zk3 <kaddachezakaria@gmail.com>
2018-05-29 11:37:34 +0200

    Commentaires

--- projet.pl
+++ projet.pl
@@ -12,21 +12,28 @@ grille([
 
 numero([1,2,3,4,5,6,7,8,9]).
 
+%Recupere l indice de la liste
 indice(L,C, N):- N is (L-1) * 9 + C.
 
+%Recupere le Ieme Element de la liste
 element([T|_], 0) :- T.
 element([_|Q], I) :- element(Q, J), I is J+1.
 
+%Test si Element dans liste
 dansListe([V|_], V).
 dansListe([_|Q], V) :- dansListe(Q, V).
 
+%concatener
 concat([], L, L).
 concat([T|Q], L, [T|R]) :- concat(Q, L, R).
 
+%ajouter Element
 ajoutElement(L, X, [X|L]).
 
+%Supprime Tete jusqu a Taille
 raccourcirTete(L, L, 0).
 raccourcirTete(R, [T|Q], Taille) :- raccourcirTete(R, Q, Taille2), Taille is Taille2 +1.
+
 %Recuperer Ligne
 recupererLigne([], _, _, 0).
 recupererLigne(R, 0, [T|Q], Taille) :- recupererLigne(L2, 0, Q, TailleM1), concat([T], L2, R), Taille is TailleM1+1.
@@ -37,4 +44,5 @@ recupererLigne(L, I, G) :- recupererLigne(L, I, G, 9).
 %Supprime Element
 supprimeElement([T|Q], X, L, R) :- ajoutElement(L, T, C), supprimeElement(Q, X, C, R).
 supprimeElement([X|Q], X, L, R) :- concat(L, Q, R), !.
-%valideLigne([T|Q], C):-
+
+valideLigne(I) :- recupererLigne(R, I, A), 
